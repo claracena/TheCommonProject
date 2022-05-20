8063dedc3014bbcf6fcf34bc69c042c57b0d8fc1
claracena <claracena@users.noreply.github.com>
2022-05-20 20:33:12 -0300

    Quick changes

--- launcher.py
+++ launcher.py
@@ -7,62 +7,6 @@
 ## Contributors:
 ###############################################################################
 
-# Imports
-# import enum
-# import template_mod as mod
-# import template_mod_2 as mod_2
-
-# modules = {'template_mod': 'mod', 'template_mod_2': 'mod_2'}
-
-# # Put your work under this line and call it from inside main():
-# def index():
-#     groups = []
-#     menu = []
-#     welcome_msg = '#################################\n'
-#     welcome_msg += '# Welcome to The Common Project #\n'
-#     welcome_msg += '#################################\n'
-#     main_question = 'Please select an option from the list:\n'
-#     selection = None
-
-#     print(welcome_msg)
-#     print(main_question)
-
-#     for module, mod_name in modules.items():
-#         modx = globals()[mod_name].head()
-#         groups.append(
-#             {'cat': modx['group'],
-#             'name': modx['name'],
-#             'desc': modx['description'],
-#             'file': module}
-#             )
-#         # menu.append(modx['group']) if modx['group'] not in groups else
-#         if modx['group'] not in menu:
-#             menu.append(modx['group'])
-
-#     # print(groups)
-    
-#     for i, item in enumerate(menu):
-#         print(f'{i + 1} - {item}')
-
-#     selection = input('\nOption (any other option to exit): ')
-
-#     try:
-#         int(selection)
-#     except:
-#         exit()
-#     else:
-#         if int(selection) - 1 < len(menu):
-#             print(f'\n{menu[int(selection) - 1]}:\n')
-#             i = 1
-#             for mod in groups:
-#                 if mod['cat'] == menu[int(selection) - 1]:
-#                     print(f'{i} - {mod["name"]}')
-#                     i += 1
-
-
-
-
-
 # Manual imports
 import importlib
 
